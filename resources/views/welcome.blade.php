4bc020969e01325f6e8c7d2d2474dab142f0e4a2
benasvj <bvajega@gmail.com>
2018-03-26 02:54:09 +0300

    sukurtas projektas, pradedamas daryti forumas

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.front')
+
+@section('content')
+    <div class="jumbotron">
+        <h1>Sveiki atvykę į Legendų Lygą!</h1>
+        <p>Gaukite naujausias žinias apie League of Legends žaidimą Lietuvoje</p>
+        <a href="" class="btn btn-primary">Sužinoti Daugiau</a>
+    </div>
+@endsection
